4603f08ae4ece78e15955526bc3cd69e19e2b4bb
JH <sktry2@naver.com>
2021-05-14 17:31:33 +0900

    클래스명 변경

--- src/main/java/lec01/mainClass.java
+++ src/main/java/lec01/mainClass.java
@@ -1,6 +1,6 @@
 package lec01;
 
-public class main  {
+public class mainClass {
     public static void main(String[] args) {
         System.out.println("헬로 월드!! ");
         System.out.println("인텔리제이 ");
