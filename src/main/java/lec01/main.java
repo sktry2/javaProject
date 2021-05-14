3c6ac787c22210fc63c354a9a016e385e4d62657
joohongpark <daleaderprofq@gmail.com>
2021-05-14 17:25:53 +0900

    주홍커밋

--- src/main/java/lec01/main.java
+++ src/main/java/lec01/main.java
@@ -4,5 +4,6 @@ public class main  {
     public static void main(String[] args) {
         System.out.println("헬로 월드!! ");
         System.out.println("인텔리제이 ");
+        System.out.println("주홍주홍");
     }
 }
